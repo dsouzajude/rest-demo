0013128edd03d10f3a91a1856bbb18db7346b29c
Jude <jude@wrapp.com>
2017-07-29 11:56:08 +0200

    Frest test run in docker

--- run_locally.sh
+++ run_locally.sh
@@ -13,3 +13,21 @@ CLIENT_PID=$!
 
 
 wait $SERVER_PID $CLIENT_PID
+
+
+
+#### Fresh test run in docker ######
+####################################
+# docker run -it \
+#            -p 8443:8443 \
+#            -p 9443:9443 \
+#            ubuntu:latest bash -c "apt-get update; \
+#            apt-get install -y python-pip git; \
+#            git clone https://github.com/dsouzajude/rest-demo.git; \
+#            cd /rest-demo/server; \
+#            python setup.py install; \
+#            cd /rest-demo/client; \
+#            python setup.py install; \
+#            myshop-server & \
+#            myshop-ui & \
+#            wait"
